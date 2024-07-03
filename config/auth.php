7c8392e4142cdeafe578e19a2977eebb3c36bbe8
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2024-07-03 14:07:17 +0700

    login

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'members',
+            'hash' => false,
+        ],
     ],
 
     /*
